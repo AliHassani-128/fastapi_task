713abcedb201a21bb93c82e0c1b1792abb8c3071
AliHassani-128 <alihassani128@yahoo.com>
2021-11-18 18:23:59 +0330

    add text model and token model

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 from pymongo import MongoClient
 from bson import ObjectId
@@ -11,11 +13,27 @@ class User(BaseModel):
     _id : ObjectId
     username : str
     password : str
+    national_code:str
 
     class Config:
         schema_extra = {
             "example": {
                 "username": "Username",
                 "password":"some password",
+                "national_code":"123456789",
             }
         }
+
+class Token(BaseModel):
+    access_token:str
+    token_type:str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class Text(BaseModel):
+    _id = ObjectId
+    text:str
+    writer:User
